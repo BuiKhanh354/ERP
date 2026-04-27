5fbacbce4062f01ce57e3b7ea017383b68672ee0
Tony <2254810078@vaa.edu.vn>
2026-04-27 20:42:10 +0700

    Dọn dẹp: xóa các file SQL tạm không cần thiết

--- SQLQuery1.sql
+++
@@ -1,7 +0,0 @@
-﻿USE ERP_DB;
-
--- Xóa bảng nếu tồn tại
-IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'projects_project_required_departments')
-BEGIN
-    DROP TABLE projects_project_required_departments;
-END
